722548a8571338409b04cabfd98c2cd21a165f62
chrismdemian <chrismdemian@gmail.com>
2026-03-13 22:14:28 -0400

    add training parameters to config

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
+# Values for dataset.py
 DATA_PATH = 'hpo-data/micro/micro/args_table.csv'
 META_PATH = 'hpo-data/dataset-meta-features/dataset-meta-features/meta-album/meta-features.json'
 TEST_SIZE = 0.2
 SEED = 42
 
+# Values for model.py
 INPUT_SIZE = 85
 HIDDEN_SIZE = 256
 OUTPUT_SIZE = 1 # Just predicting accuracy
 DROPOUT = 0.2 # To prevent overfitting
+
+# Values for train.py
+LEARNING_RATE = 0.001
+BATCH_SIZE = 64
+EPOCHS = 100
