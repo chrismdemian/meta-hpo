7e7fc624594954384804dc9dd432b067bb3387d5
chrismdemian <chrismdemian@gmail.com>
2026-03-13 22:11:18 -0400

    write out model code with 3 linear layers

--- model.py
+++ model.py
@@ -1,3 +1,23 @@
 import torch
 import torch.nn as nn
 from config import INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE, DROPOUT
+
+# Build a class for the neural network
+class HPOModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        # 3 linear layers with dropout and relu activation
+        self.network = nn.Sequential(
+            nn.Linear(INPUT_SIZE, HIDDEN_SIZE),
+            nn.ReLU(),
+            nn.Dropout(DROPOUT),
+            nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
+            nn.ReLU(),
+            nn.Dropout(DROPOUT),
+            nn.Linear(HIDDEN_SIZE, OUTPUT_SIZE)
+        )
+    
+    # Forward method
+    def forward(self, x):
+        return self.network(x)
