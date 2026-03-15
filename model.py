842cf70de9e56b865be7111f57c9c65ca4a0937f
chrismdemian <chrismdemian@gmail.com>
2026-03-14 21:16:37 -0400

    test different hyperparameters and architecture

--- model.py
+++ model.py
@@ -10,9 +10,11 @@ class HPOModel(nn.Module):
         # 3 linear layers with dropout, batch normalization, and relu activation
         self.network = nn.Sequential(
             nn.Linear(INPUT_SIZE, HIDDEN_SIZE),
+            nn.BatchNorm1d(HIDDEN_SIZE),
             nn.ReLU(),
             nn.Dropout(DROPOUT),
             nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
+            nn.BatchNorm1d(HIDDEN_SIZE),
             nn.ReLU(),
             nn.Dropout(DROPOUT),
             nn.Linear(HIDDEN_SIZE, OUTPUT_SIZE)
